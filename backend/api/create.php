a03ec5887048519df2ad2156129f3968baa456b1
Armando <josearmandovalenciam@gmail.com>
2019-12-28 13:10:58 -0600

    added base backend function

---
+++ backend/api/create.php
@@ -0,0 +1,54 @@
+<?php
+require 'database.php';
+
+// Get the posted data.
+$postdata = file_get_contents("php://input");
+
+if(isset($postdata) && !empty($postdata))
+{
+  // Extract the data.
+  $request = json_decode($postdata);
+
+
+  // Validate.
+  //if(trim($request->number) === '' || (float)$request->amount < 0)
+  //{
+  //  return http_response_code(400);
+  //}
+
+  // Sanitize.
+  //$number = mysqli_real_escape_string($con, trim($request->number));
+  //$amount = mysqli_real_escape_string($con, (int)$request->amount);
+
+  $id_catalog = $request->id_catalog;
+  $catalog_name = $request->catalog_name;
+  $availability = $request->availability;
+  $catalog_price = $request->catalog_price;
+
+  // Create.
+  //$sql = "INSERT INTO `policies`(`id`,`number`,`amount`) VALUES (null,'{$number}','{$amount}')";
+
+  $sql = "INSERT INTO 'catalogs'('id_catalog', 'catalog_name', 'availability', 'catalog_price') VALUES ('{$id_catalog}', '{$catalog_name}', '{$availability}', '{$catalog_price}')";
+
+  if(mysqli_query($con, $sql)){
+      http_response_code(201);
+  }else{
+      http_response_code(400);
+  }
+/*
+  if(mysqli_query($con,$sql))
+  {
+    http_response_code(201);
+    $policy = [
+      'number' => $number,
+      'amount' => $amount,
+      'id'    => mysqli_insert_id($con)
+    ];
+    echo json_encode($policy);
+  }
+  else
+  {
+    http_response_code(422);
+  }
+*/
+}
